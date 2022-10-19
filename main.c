ea55c4197a723d519eba82d5d0454b50ca3941f0
Andrii <andpris527@gmail.com>
2022-10-19 21:27:44 +0300

    add checkout

--- main.c
+++ main.c
@@ -3,43 +3,51 @@
 
 int main() {
     float x1, x2, dx, e, x, radian, d, sinx, cosx;
-    short n;
-    printf("Low border:");
-    if(scanf("%f", &x1) <= 0) {
-        printf("Invalid data");
-        return 0;
-    };
-    printf("Top border:");
-    if(scanf("%f", &x2) <= 0) {
-        printf("Invalid data");
-        return 0;
-    };
-    printf("Step:");
-    if(scanf("%f", &dx) <= 0) {
-        printf("Invalid data");
-        return 0;
-    };
-    printf("Precision:");
-    if(scanf("%f", &e) <= 0) {
-        printf("Invalid data");
-        return 0;
-    };
+    short n, check;
 
-    printf("X\t\tComputed value\t\tTable value\t\tDifference\n");
-    x = x1;
-    while(x <= x2) {
-        radian = x*M_PI/180;
+    printf("Enter 1 to calculate sinus or 2 to calculate cosinus:");
+    if(scanf("%d", &check) <= 0) {
+        printf("Invalid data");
+        return 0;
+    };
+    if(check == 1) {
+        printf("Low border:");
+        if (scanf("%f", &x1) <= 0) {
+            printf("Invalid data");
+            return 0;
+        };
+        printf("Top border:");
+        if (scanf("%f", &x2) <= 0) {
+            printf("Invalid data");
+            return 0;
+        };
+        printf("Step:");
+        if (scanf("%f", &dx) <= 0) {
+            printf("Invalid data");
+            return 0;
+        };
+        printf("Precision:");
+        if (scanf("%f", &e) <= 0) {
+            printf("Invalid data");
+            return 0;
+        };
 
-        d = sinx = radian;
-        n = 1;
-        while(fabs(d) >= e) {
-            d *= (-radian*radian) / ((n+1)*(n+2));
-            sinx += d;
-            n += 2;
+        printf("\nX\t\tComputed value\t\tTable value\t\tDifference\n");
+        x = x1;
+        while (x <= x2) {
+            radian = x * M_PI / 180;
+
+            d = sinx = radian;
+            n = 1;
+            while (fabs(d) >= e) {
+                d *= (-radian * radian) / ((n + 1) * (n + 2));
+                sinx += d;
+                n += 2;
+            }
+
+            printf("%.2f\t\t%.3f\t\t\t%.3f\t\t\t%.3f\n", x, sinx, sin(radian), fabs(sinx - sin(radian)));
+            x += dx;
         }
-
-        printf("%.2f\t\t%.3f\t\t\t%.3f\t\t\t%.3f\n", x, sinx, sin(radian), fabs(sinx-sin(radian)));
-        x += dx;
     }
     return 0;
 }
